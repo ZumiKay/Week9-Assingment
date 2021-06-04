e2f4fe5a35762cf6a2a9d705e2a6ec8dce0b98d9
ZumiLock <zumilock011@gmail.com>
2021-06-04 20:45:05 +0700

    Lastest commit

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,29 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/property-grid' , function (){
+    return view('property-grid');
+});
+Route::get('/Property-single' , function (){
+    return view('Property-single');
+});
+Route::get('/Agent-grid', function () {
+    return view('Agent-grid');
+});
+Route::get('/Agent-single', function () {
+    return view('Agent-single');
+});
+Route::get('/blog-grid', function () {
+    return view('blog-grid');
+});
+Route::get('/Blog-single', function () {
+    return view('Blog-single');
+});
+Route::get('/contact', function () {
+    return view('contact');
 });
